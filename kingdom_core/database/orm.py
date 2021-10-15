58771910085a8e321dc373c780ec14201eb5faf0
William Abreu <william@t10.digital>
2021-10-15 16:30:08 -0300

    refactor(orm): split in half the start_mappers

--- kingdom_core/database/orm.py
+++ kingdom_core/database/orm.py
@@ -1,49 +1,35 @@
 from importlib import import_module
 from importlib.machinery import SourceFileLoader
-from typing import Iterable, Optional
 
 from sqlalchemy import MetaData
 
 from kingdom_core.utils import files
 
 
-def _load_start_mappers_auto(src_dir: str, metadata: MetaData) -> None:
+def auto_start_mappers(src_dir: str) -> MetaData:
+    """Discover orm.py files and run sqlalchemy's mappers automatically."""
+    metadata = MetaData()
     orm_files = files.find("orm.py", src_dir)
+
     for path in orm_files:
         module = SourceFileLoader("start_mappers", path).load_module()
         module.start_mappers(metadata)  # type: ignore
 
+    return metadata
 
-def _load_start_mappers_manual(
-    modules: Iterable[str], metadata: MetaData
-) -> None:
+
+def start_mappers(*modules: str) -> MetaData:
+    """Update metadata reference and run sqlalchemy's mappers, classical way.
+
+    :param modules: enumeration of the modules (dot-case format).
+
+    Example:
+    >>> start_mappers('my.module.1.orm', 'my.module.2.orm')
+    """
+    metadata = MetaData()
+
     for module_name in modules:
         module = import_module(module_name)
         module.start_mappers(metadata)  # type: ignore
-
-
-def start_mappers(
-    src_dir: Optional[str] = None,
-    modules: Optional[Iterable[str]] = None,
-) -> MetaData:
-    """Update metadata reference and run sqlalchemy's mappers, classical way.
-
-    :param src_dir: if wanted load all start_mappers automatically, inform this
-    single path.
-    :param modules: if wanted load only specific start_mappers, inform a
-    list containing the modules (dot-case format).
-    """
-    if not src_dir and not modules:
-        raise TypeError("One argument must be specified")
-
-    if src_dir and modules:
-        raise TypeError("The arguments are mutually exclusive")
-
-    metadata = MetaData()
-
-    if src_dir:
-        _load_start_mappers_auto(src_dir, metadata)
-    else:
-        _load_start_mappers_manual(modules, metadata)
 
     return metadata
