1ceeede047cf1143760bf1277d1a626c2fa9bd9b
William Abreu <william@t10.digital>
2021-10-18 18:54:36 -0300

    doc(orm): changed the docstring format

--- kingdom_core/database/orm.py
+++ kingdom_core/database/orm.py
@@ -7,7 +7,14 @@ from kingdom_core.utils import files
 
 
 def auto_start_mappers(src_dir: str) -> MetaData:
-    """Discover orm.py files and run sqlalchemy's mappers automatically."""
+    """Discover orm.py files and run sqlalchemy's mappers automatically.
+
+    Args:
+        src_dir: Absolute path where is located the source to search.
+
+    Returns:
+        SQLAlchemy MetaData.
+    """
     metadata = MetaData()
     orm_files = files.find("orm.py", src_dir)
 
@@ -21,10 +28,16 @@ def auto_start_mappers(src_dir: str) -> MetaData:
 def start_mappers(*modules: str) -> MetaData:
     """Update metadata reference and run sqlalchemy's mappers, classical way.
 
-    :param modules: enumeration of the modules (dot-case format).
-
-    Example:
     >>> start_mappers('my.module.1.orm', 'my.module.2.orm')
+
+    Args:
+        modules: Enumeration of the modules (dot-case format).
+
+    Returns:
+        SQLAlchemy MetaData.
+
+    Raises:
+        ModuleNotFoundError: If the module is wrong or it doesn't exist.
     """
     metadata = MetaData()
 
