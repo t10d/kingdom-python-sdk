eb6090ddded5d751fdb0807764fbfd5963624955
William Abreu <william@t10.digital>
2021-10-19 18:22:57 -0300

    feat(domain): added base messages models (stale)

---
+++ kingdom_sdk/domain/model/command.py
@@ -0,0 +1,5 @@
+from kingdom_sdk.domain.model.message import Message
+
+
+class Command(Message):
+    pass
---
+++ kingdom_sdk/domain/model/event.py
@@ -0,0 +1,5 @@
+from kingdom_sdk.domain.model.message import Message
+
+
+class Event(Message):
+    pass
