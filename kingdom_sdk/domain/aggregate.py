0a44c67e8c9a0e7d168bcaf20b67f2d3210af6b3
William Abreu <william@t10.digital>
2021-10-21 14:57:59 -0300

    fix(domain): added missing aggregate constructor

--- kingdom_sdk/domain/aggregate.py
+++ kingdom_sdk/domain/aggregate.py
@@ -1,5 +1,7 @@
 from abc import ABC
+from datetime import datetime
 from typing import List
+from uuid import UUID
 
 from kingdom_sdk.domain.entity import Entity
 from kingdom_sdk.domain.message import Event
@@ -9,6 +11,17 @@ class Aggregate(Entity, ABC):
     """Base class for aggregates."""
 
     _events: List[Event]
+
+    def __init__(
+        self,
+        id: UUID,  # noqa
+        version: int,
+        is_discarded: bool,
+        registered_at: datetime,
+        updated_at: datetime,
+    ):
+        super().__init__(id, version, is_discarded, registered_at, updated_at)
+        self._events = []
 
     def add_events(self, *events: Event) -> None:
         self._check_not_discarded()
