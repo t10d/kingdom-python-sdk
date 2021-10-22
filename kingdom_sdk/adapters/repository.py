23d0dcfa1f0171c6f30a57a0ef0190393ac35f5b
William Abreu <william@t10.digital>
2021-10-22 15:50:37 -0300

    feat(adapters): added base repository implementation

---
+++ kingdom_sdk/adapters/repository.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+from abc import ABC
+from typing import List, Optional, Set, Type
+
+from sqlalchemy.orm import Session
+
+from kingdom_sdk.domain.aggregate import Aggregate
+from kingdom_sdk.ports.repository import AbstractRepository
+from kingdom_sdk.ports.types import PrimaryKey_T
+
+
+class BaseRepository(AbstractRepository, ABC):
+    """Generic repository.
+
+    You only need to extend it and override the static attribute _model.
+
+    >>> class MyRepositoy(BaseRepository):
+    ...     _model = ...
+    """
+
+    _model: Type[Aggregate]
+    _session: Session
+    _seen: Set[Aggregate]
+
+    def __init__(self, session: Session) -> None:
+        self._session = session
+        self._seen: Set[Aggregate] = set()
+
+    def get(self, id: PrimaryKey_T) -> Optional[Aggregate]:  # noqa
+        aggregate = self._get(id)
+        if aggregate:
+            self._seen.add(aggregate)
+        return aggregate
+
+    def _add(self, aggregate: Aggregate) -> None:
+        self._seen.add(aggregate)
+        self._session.add(aggregate)
+
+    def _list(self) -> List[Aggregate]:
+        return self._session.query(self._model).all()  # type: ignore
+
+    def _get(self, id: PrimaryKey_T) -> Optional[Aggregate]:  # noqa
+        return (  # type: ignore
+            self._session.query(self._model)
+            .filter(
+                self._model._id == id,  # noqa
+                self._model._is_discarded._is(False),  # type: ignore  # noqa
+            )
+            .first()
+        )
