e06f35b353462de5bb1c2396b4750db5a1428f4c
William Abreu <william@t10.digital>
2021-10-26 10:34:23 -0300

    tests: improved assertion test for native query

--- tests/context/integration/test_query.py
+++ tests/context/integration/test_query.py
@@ -1,15 +1,17 @@
 from uuid import uuid4
 
+from sqlalchemy.engine import CursorResult
+
 from kingdom_sdk.adapters.query import DQLInterface
 
 
 def test_query_missing_params(uow, query_path):
     query = DQLInterface(query_path)
     result = query.execute(uow, id=uuid4().hex)
-    assert result
+    assert isinstance(result, CursorResult)
 
 
 def test_query_full_params(uow, query_path):
     query = DQLInterface(query_path)
     result = query.execute(uow, id=uuid4().hex, name="%a%")
-    assert result
+    assert isinstance(result, CursorResult)
