59ac2d811d93be58430a81f95b6defb03b2efbcd
William Abreu <william@t10.digital>
2021-11-11 10:07:29 -0300

    refactor(domain): improved command/event model

--- kingdom_sdk/domain/message.py
+++ kingdom_sdk/domain/message.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
-from abc import ABC
+from abc import ABC, abstractmethod
 from dataclasses import asdict, dataclass
 from datetime import datetime
 from typing import Any, Dict
 from uuid import UUID
 
 from kingdom_sdk.domain.value_object import ValueObject
-from kingdom_sdk.utils import loader, time
+from kingdom_sdk.utils import loader
 
 
 @dataclass(frozen=True)
@@ -17,32 +17,53 @@ class Message(ValueObject, ABC):
     Messages are value objects, so they are equality comparable.
 
     Args:
-        type: ...
-        kind: ...
         raised_at: Timestamp when the message had generated.
         delay: ...
     """
 
-    type: str
-    kind: str
     raised_at: datetime
     delay: int
 
 
-@dataclass(frozen=True)
 class Command(Message, ABC):
-    """Base domain command."""
+    """Base domain command.
+
+    You must implement this class and annotate it with @dataclass(frozen=True).
+
+    >>> @dataclass(frozen=True)
+    ... class MyCommand(Command):
+    ...     @abstractmethod
+    ...     def create(self, ...) -> MyCommand:
+    ...         ...
+    """
+
+    @classmethod
+    @abstractmethod
+    def create(cls, **kwargs: Any) -> Command:
+        raise NotImplementedError
 
 
-@dataclass(frozen=True)
 class Event(Message, ABC):
     """Base domain event.
+
+    You must implement this class and annotate it with @dataclass(frozen=True).
+
+    >>> @dataclass(frozen=True)
+    ... class MyEvent(Event):
+    ...     @abstractmethod
+    ...     def create(self, ...) -> MyEvent:
+    ...         ...
 
     Args:
         raised_by: The originating aggregate root id.
     """
 
     raised_by: UUID
+
+    @classmethod
+    @abstractmethod
+    def create(cls, **kwargs: Any) -> Command:
+        raise NotImplementedError
 
 
 @dataclass(frozen=True)
@@ -51,23 +72,14 @@ class PersistentMessage:
     classname: str
     data: Dict[str, Any]
 
+    @classmethod
+    def create(cls, message: Message) -> PersistentMessage:
+        return cls(
+            message.__class__.__module__,
+            message.__class__.__name__,
+            asdict(message),
+        )
+
     def load_object(self) -> Message:
         cls = loader.object_from_module(self.module, self.classname)
         return cls(**self.data)  # type: ignore
-
-
-if __name__ == "__main__":
-    # Example to store the data in JSON and load it as object again.
-
-    msg = Message(
-        type="GenericMessage",
-        kind="Event",
-        raised_at=time.generate_now(),
-        delay=1,
-    )
-
-    pm = PersistentMessage(
-        msg.__class__.__module__, msg.__class__.__name__, asdict(msg)
-    )
-
-    print(pm.load_object())
