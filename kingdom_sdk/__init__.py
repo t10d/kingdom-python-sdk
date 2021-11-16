9f4bcc184c0e42f2085e44a7aa7ba03a887f2e97
William Abreu <william@t10.digital>
2021-11-16 18:43:17 -0300

    doc: updated changelog

--- kingdom_sdk/__init__.py
+++ kingdom_sdk/__init__.py
@@ -3,7 +3,7 @@ import os
 # Define the package's version.
 # - Is used for indexing and distributing the package
 # - Follow the conventions defined by PEP440
-__version__ = "0.3.0dev1"
+__version__ = "0.3.0"
 
 
 def _get_base_dir() -> str:
